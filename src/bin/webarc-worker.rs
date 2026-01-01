e0531fa731e00973c585be26a0a2c27e45a38c35
Naomi Phillips <naomi@nep.soy>
2026-01-01 00:20:01 -0500

    Add empty web server boilerplate to worker server

--- src/bin/webarc-worker.rs
+++ src/bin/webarc-worker.rs
@@ -1,5 +1,24 @@
+#[macro_use]
+extern crate log;
+
+use actix_web::{App, HttpServer, Responder, get};
+
+#[get("/version")]
+async fn version() -> impl Responder {
+    format!("{}", env!("CARGO_PKG_VERSION"))
+}
+
+async fn server(sock: impl std::net::ToSocketAddrs) -> std::io::Result<()> {
+    HttpServer::new(|| App::new().service(version))
+        .bind(sock)?
+        .run()
+        .await
+}
+
 #[tokio::main]
 async fn main() {
-    let sum = webarc::add(4, 2);
-    println!("worker: {sum}");
+    pretty_env_logger::init();
+
+    let sock = ("127.0.0.1", 8081);
+    let _ = server(sock).await;
 }
